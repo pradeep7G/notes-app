76a1eaa7cb62743bea1b19731cdfd36b0a3ad6c8
pradeep7G <pradeep7chinna@gmail.com>
2021-06-14 16:32:22 +0530

    fixing deploying issues

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const unknowEndpoint=(req,res)=>{
 app.use(unknowEndpoint);
 app.use(errorHandler);
 
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 3001;
 app.listen(PORT,()=>{
   console.log(`Server running on port ${PORT}`);
 })
