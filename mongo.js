4b90abb0f9b8d8b928e7f78b160fa4e235792133
pradeep7G <pradeep7chinna@gmail.com>
2021-06-14 14:16:38 +0530

    completed practice upto ex-3.15 learned alot and learned that there is a shitton to learn

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose=require('mongoose');
+
+if(process.argv.length < 3)
+{
+    console.log('Please provide teh password as an argument: node mongo.js <password>');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url=`mongodb+srv://fso_app:curious25@fsoapp.crvxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify: false,useCreateIndex:true});
+
+const noteSchema=new mongoose.Schema({
+    content: String,
+    date:Date,
+    important:Boolean,
+});
+
+const Note=mongoose.model('Note',noteSchema);
+
+// const note=new Note({
+//     content: 'Callback functions suck',
+//     date: new Date(),
+//     important: true,
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note =>{
+        console.log(note);
+    });
+    mongoose.connection.close();
+})
+
+// Note.find({content:'HTML is Easy'}).then(result =>{
+//     console.log(result);
+//     mongoose.connection.close();
+// })
+
+// note.save().then(result =>{
+//     console.log('note saved!');
+//     mongoose.connection.close();
+// })
