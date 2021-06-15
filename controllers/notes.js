8f16e5c6be6f1d803c7cfb930a02650ed1ff8226
pradeep7G <pradeep7chinna@gmail.com>
2021-06-15 09:11:55 +0530

    changed the structure of the backend moved app routes and middle wares etc into their modules

---
+++ controllers/notes.js
@@ -0,0 +1,65 @@
+const notesRouter=require('express').Router()
+const Note=require('../models/note')
+
+notesRouter.get('/',(req,res) => {
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
+})
+
+notesRouter.get('/:id',(req,res,next) => {
+  Note.findById(req.params.id)
+    .then( note => {
+      if(note)
+      {
+        res.json(note)
+      }
+      else{
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.post('/',(req,res,next) => {
+
+  const body=req.body
+  const note=new Note({
+    content:body.content,
+    important:body.important || false,
+    date:new Date(),
+  })
+
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      res.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
+
+})
+
+notesRouter.delete('/:id',(req,res,next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.put('/:id',(req,res,next) => {
+  const body=req.body
+
+  const note={
+    content:body.content,
+    important:body.important,
+  }
+
+  Note.findByIdAndUpdate(req.params.id,note,{ new:true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+module.exports=notesRouter
